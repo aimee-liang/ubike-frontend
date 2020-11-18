f5b7ca8355de54015f8aae65aa81947478e67bf9
aimee-liang <aimeeeliang@gmail.com>
2020-11-18 16:58:52 -0500

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import React from "react"
-import {Route, Switch, withRouter} from "react-router-dom"
+import { Route, Switch, withRouter } from "react-router-dom"
 import ProfilePage from "./Containers/ProfilePage"
 import Home from "./Containers/Home"
+import SideBar from "./Containers/SideBar"
+
 class App extends React.Component{  
   
   render(){
     return (
       <>
+      <SideBar />
+      
       <Switch>
         <Route path ="/home" render={()=> <Home /> } />
         <Route path="/profile" render={() => <ProfilePage /> } />
---
+++ src/Containers/SideBar.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+
+export default class SideBar extends React.Component{
+    render(){
+        return(
+            <>
+            <NavLink to="/Home">
+                Home
+            </NavLink>
+
+            <NavLink to="/profile">
+                Profile
+            </NavLink>
+
+            {/* <NavLink to="/signout">
+                Sign out
+            </NavLink> */}
+            </>
+        )
+    }
+}
