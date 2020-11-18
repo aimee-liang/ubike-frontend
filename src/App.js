42c57c8416c285ee482eef8aea9fce4053716123
aimee-liang <aimeeeliang@gmail.com>
2020-11-18 15:34:44 -0500

    Added home page route to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from "react"
 import {Route, Switch, withRouter} from "react-router-dom"
 import ProfilePage from "./Containers/ProfilePage"
+import Home from "./Containers/Home"
 class App extends React.Component{  
   
   render(){
     return (
       <>
       <Switch>
-
+        <Route path ="/home" render={()=> <Home /> } />
         <Route path="/profile" render={() => <ProfilePage /> } />
       </Switch>
       </>
