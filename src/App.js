3c5c1c0dd1801f00398808f1138f1fe5ff3d9d26
aimee-liang <aimeeeliang@gmail.com>
2020-11-18 15:30:55 -0500

    Added profile page route to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from "react"
-import {Route, Switch, withRouter} from 
-
+import {Route, Switch, withRouter} from "react-router-dom"
+import ProfilePage from "./Containers/ProfilePage"
 class App extends React.Component{  
   
   render(){
     return (
       <>
+      <Switch>
+
+        <Route path="/profile" render={() => <ProfilePage /> } />
+      </Switch>
       </>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
