3a91f1368f1c37bac7736bf989ccb7aa3d2bff30
aimee-liang <aimeeeliang@gmail.com>
2020-11-29 00:31:47 -0500

    Trying to solve markers in map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,36 @@
 import React from "react"
+import {PureComponent} from "react"
 import ReactMapGL, {Marker} from "react-map-gl"
 
 // creating list of bike shelters
 const shelters=[
-    
+    {name: ""},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
 ]
+// class Markers extends PureComponent{
+//     render(){
+//         const data={this.props}
+//         return data.map(
+//             shelter => <Marker key={shelter.name} longitude={shelter.longitude} latitude={shelter.latitude}><img src=""/></Marker>
+//         )
+//     }
+// }
 
 class Map extends React.Component{
     state={
@@ -16,12 +42,14 @@ class Map extends React.Component{
             zoom: 10
         }
     }
+
     render(){
     return(
         <div className="map">
             <ReactMapGL {...this.state.viewport} mapStyle="mapbox://styles/lianga/ckhppbi8209vs19qtm3yamjmt"
             onViewportChange={(viewport => this.setState({viewport}))}
             mapboxApiAccessToken="pk.eyJ1IjoibGlhbmdhIiwiYSI6ImNraG9kODF6djAybDkyd3FsMnhxcG8wMmsifQ.5Z8qlJahYdbW_JlGhhs1hQ">
+                {/* <Markers data={shelters} /> */}
             </ReactMapGL>
         </div>
     )}
