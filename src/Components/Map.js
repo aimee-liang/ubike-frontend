f2572222f5c33e19ed605c67225a5c6af4be7422
aimee-liang <aimeeeliang@gmail.com>
2020-12-01 10:59:22 -0500

    Working on shelter information

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,24 +4,24 @@ import ReactMapGL, {Marker} from "react-map-gl"
 
 // creating list of bike shelters
 const shelters=[
-    {name: ""},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+    {name: "Dekalb Ave & Flatbush Ave", longitude: -74.170972, latitude: 40.560158},
+    {name: "Grahan Ave & Metropolitan Ave", longitude: -74.066198, latitude: 40.590507},
+    {name: "Kings Hwy & E. 15th St", longitude:, latitude: },
+    {name: "4th Ave & 36th St", longitude: -74.004185, latitude: 40.654291},
+    {name: "Surf Ave & Stillwell Ave", longitude:, latitude: },
+    {name: "E. Kingbridge Rd & Briggs Ave", longitude:, latitude: },
+    {name: "Grand Concourse & E.Tremont Ave", longitude:, latitude: },
+    {name: "E. 17th St & Broadway", longitude:, latitude: },
+    {name: "W. 23rd St & 6th Ave", longitude:, latitude: },
+    {name: "W. 23rd St & 8th Ave", longitude: -73.998087, latitude: 40.745228},
+    {name: "W. 33rd St & 8th Ave", longitude: -73.993644, latitude: 40.751326},
+    {name: "Rutgers St & East Broadway", longitude:, latitude: },
+    {name: "Jackson Ave & 50th Ave", longitude:, latitude: },
+    {name: "31st St & 30th Ave", longitude:, latitude: },
+    {name: "Lefferts Blvd & Liberty Ave", longitude:, latitude: },
+    {name: "71st Ave & Austin St", longitude:, latitude: },
+    {name: "Father Capodanno Blvd & Sand Lane", longitude:, latitude: },
+    {name: "Richmond Ave & Wainwright Ave", longitude:, latitude: }
 ]
 // class Markers extends PureComponent{
 //     render(){
