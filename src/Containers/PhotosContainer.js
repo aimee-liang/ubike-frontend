8d121d7b5cc03cdeebdce70e6698bfe41eabcaa1
aimee-liang <aimeeeliang@gmail.com>
2020-12-01 21:00:55 -0500

    Added button to Photos Container

--- src/Containers/PhotosContainer.js
+++ src/Containers/PhotosContainer.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Button from "@material-ui/core/Button"
 
 const PhotosContainer = (props) => {
     return (
         <div>
             <img alt=""src={props.photos ? props.photos.photos : "/placeholder.jpg" }  className="default-pic"  />
             <br></br>
-            <button>Upload Photos</button>
+            <Button variant="contained" color="primary" component="span">Upload Photos</Button>
         </div>
     )
 }
