f09295e2640426e6aa3d90674aa959229f3d8a36
aimee-liang <aimeeeliang@gmail.com>
2020-12-02 14:24:44 -0500

    Fixed button for show page

--- src/Containers/PhotosContainer.js
+++ src/Containers/PhotosContainer.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Button from "@material-ui/core/Button"
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 
 const PhotosContainer = (props) => {
     return (
         <div>
             <img alt=""src={props.photos ? props.photos.photos : "/placeholder.jpg" }  className="default-pic"  />
             <br></br>
-            <Button variant="contained" color="primary" component="span">Upload Photos</Button>
+            <Button variant="contained" color="primary" component="span"><CloudUploadIcon/> &nbsp; Upload Photos</Button>
         </div>
     )
 }
