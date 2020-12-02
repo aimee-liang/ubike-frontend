4f59caa8609fea7a65b95dbf57baa91316871190
aimee-liang <aimeeeliang@gmail.com>
2020-12-01 22:52:52 -0500

    Sign up complete

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -36,30 +36,36 @@ class Signup extends React.Component{
         return(
             <div className="login-background">
 
-                <div className="login-body">
+                <div className="signup-body">
 
                     <div className="logo">
                         <img alt="" src ="/ubike.png" />
                     </div>
 
 
-                    <form className="login-form" noValidate autoComplete="off" onSubmit={this.localLoginHandler}></form>
+                    <form className="signup-form" noValidate autoComplete="off" onSubmit={this.localSignupHandler}>
 
-                        <h4>Join ubike</h4>
-            
-                            <form onSubmit={this.localSignupHandler}>
-                                <TextField required id="standard-required" label="Name" input type="text" input="true" name="name" value={this.state.name} onChange={this.changeHandler} />
-                                <TextField required id="standard-required" label="Username" input type="text" input="true" name="username" value={this.state.username} onChange={this.changeHandler} />
-                                <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.changeHandler} />
-                                <input type="text" name="bike" placeholder="Bike details" value={this.state.bicycle} onChange={this.changeHandler} />
-                                <input type="text" name="bio" placeholder="Bio" value={this.state.bio} onChange={this.changeHandler} />
-                                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler} />
-                                <br></br>
-                                <br></br>
-                                <p>Already have an account?</p><NavLink to="/login">Sign in</NavLink>
-                                <br></br>
-                                <Button variant="contained" color="primary" type="submit" value="sign up">Sign up </Button>
-                            </form>
+                        <h3>Join ubike</h3>
+                        <br></br>
+                        <TextField required id="standard-required" label="Name" input type="text" input="true" name="name" value={this.state.name} onChange={this.changeHandler} />
+                        <br></br>
+                        <TextField required id="standard-required" label="Username" input type="text" input="true" name="username" value={this.state.username} onChange={this.changeHandler} />
+                        <br></br>
+                        <TextField required id="standard-required" label="Email" input type="text" input="true" name="email" value={this.state.email} onChange={this.changeHandler} />
+                        <br></br>
+                        <TextField required id="standard-required" label="Bicycle info" input type="text" input="true" name="bike" placeholder="Bike details" value={this.state.bicycle} onChange={this.changeHandler} />
+                        <br></br>
+                        <TextField required id="standard-required" label="About Me" input type="text" name="bio" value={this.state.bio} onChange={this.changeHandler} />
+                        <br></br>
+                        <TextField required id="standard-required" label="Password" input type="password" input="true" name="password" value={this.state.password} onChange={this.changeHandler} />
+                        <br></br>
+                        <br></br>
+                        <NavLink to="/login">Sign in</NavLink>
+                        <br></br>
+                        <br></br>
+                        <Button variant="contained" color="primary" type="submit" value="sign up">Sign up </Button>
+
+                    </form>
                 </div>
             </div>
         )
