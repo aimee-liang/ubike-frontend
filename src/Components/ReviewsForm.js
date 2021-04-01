1178cc994c1520ffe042fd2644248c5b38a3b608
aimee-liang <aimeeeliang@gmail.com>
2021-03-31 22:43:33 -0400

    ReviewsForm with hooks

--- src/Components/ReviewsForm.js
+++ src/Components/ReviewsForm.js
@@ -3,31 +3,38 @@ import Button from "@material-ui/core/Button"
 import AddCommentIcon from '@material-ui/icons/AddComment';
 import TextField from "@material-ui/core/TextField"
 
-export default class ReviewsForm extends React.Component{
-    state={
-        comment: ""
+const ReviewsForm = props => {
+// export default class ReviewsForm extends React.Component{
+
+    const [comment, setComment] = useState("")
+    // state={
+    //     comment: ""
+    // }
+
+    const changeHandler = (e) => {
+        this.setState({...comment, [e.target.name]: e.target.value})
     }
 
-    changeHandler = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    localSubmitComments = (e) => {
+    const localSubmitComments = (e) => {
         e.preventDefault()
-        this.props.submitComments(this.state.comment)
-        this.setState(()=> ({
-            comment: ""
-        }))
+        props.submitComments(comment)
+
+        setComment("")
+        // this.setState(()=> ({
+        //     comment: ""
+        // }))
     }
 
-    render(){
+    // render(){
         return(
-            <form onSubmit={this.localSubmitComments}>
-                <TextField id="outlined-basic" variant="outlined" input type="textarea" name="comment" value={this.state.comment} label="Write a comment..." onChange={this.changeHandler} />
+            <form onSubmit={localSubmitComments}>
+                <TextField id="outlined-basic" variant="outlined" input type="textarea" name="comment" value={comment.comment} label="Write a comment..." onChange={changeHandler} />
                 <br></br>
                 <br></br>
                 <Button variant="contained" color="primary" type="submit" value="Submit comment"> <AddCommentIcon/> &nbsp;Submit</Button>
             </form>
         )
-    }
+    // }
 }
+
+export default ReviewsForm
