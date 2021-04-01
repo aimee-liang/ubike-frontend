22fe6d44151cbbe8b7be7bf318fd19fd87815ec9
aimee-liang <aimeeeliang@gmail.com>
2021-03-31 22:57:59 -0400

    Removed this in CheckInSpan

--- src/Components/CheckInSpan.js
+++ src/Components/CheckInSpan.js
@@ -6,7 +6,7 @@ const CheckInSpan = (props) => {
     // render(){
         return(
             <span>
-                <p>Parked at {this.props.checkIn.location} since: {this.props.checkIn.datetime}</p>
+                <p>Parked at {props.checkIn.location} since: {props.checkIn.datetime}</p>
             </span>
         )
 
